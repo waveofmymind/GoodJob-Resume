2c53061af66202420f45896e064f1885c505a557
waveofmymind <sjun0913@gmail.com>
2023-07-20 22:11:04 +0900

    Initial commit

---
+++ src/main/kotlin/com/goodjob/resume/GoodjobResumeApplication.kt
@@ -0,0 +1,11 @@
+package com.goodjob.resume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoodjobResumeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoodjobResumeApplication>(*args)
+}
---
+++ src/test/kotlin/com/goodjob/resume/GoodjobResumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goodjob.resume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoodjobResumeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
