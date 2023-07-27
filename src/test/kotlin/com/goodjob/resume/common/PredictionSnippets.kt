f23b2e8b71cdc39311ef74b8259b2662d9f05d6a
waveofmymind <sjun0913@gmail.com>
2023-07-27 20:49:58 +0900

    feat: 스니펫 함수 추가

--- src/test/kotlin/com/goodjob/resume/common/PredictionSnippets.kt
+++ src/test/kotlin/com/goodjob/resume/common/PredictionSnippets.kt
@@ -1,15 +1,48 @@
 package com.goodjob.resume.common
 
+import com.goodjob.resume.application.facade.FindPredictionResponse
 import com.goodjob.resume.application.facade.RegisterPredictionCommand
-import com.goodjob.resume.domain.ServiceType
+import com.goodjob.resume.domain.*
+import java.time.LocalDateTime
 
 object PredictionSnippets {
 
+    fun prediction() : Prediction {
+        val prediction = Prediction(
+            memberId = 1L,
+            serviceType = ServiceType.EXPECTED_ADVICE
+        )
+        prediction.addTitle(listOf("title1", "title2"))
+        prediction.addContent(listOf("content1", "content2"))
+        return prediction
+    }
     fun registerPredictionCommand(titleList: List<String>, contentList: List<String>) =
         RegisterPredictionCommand(
             memberId = 1L,
             titles = titleList,
             contents = contentList,
             serviceType = ServiceType.EXPECTED_ADVICE
+        )
+
+    fun findPredictionResponse(titleList: List<String>, contentList: List<String>) =
+        FindPredictionResponse(
+            id = 1L,
+            titles(),
+            contents(),
+            serviceType = ServiceType.EXPECTED_ADVICE,
+            createdDate = LocalDateTime.of(2023, 1, 1, 0, 0, 0),
+        )
+
+    private fun titles() =
+        Titles(
+            listOf(
+                Title.of("title1", prediction()),
+            )
+        )
+    private fun contents() =
+        Contents(
+            listOf(
+                Content.of("content1", prediction()),
+            )
         )
 }
